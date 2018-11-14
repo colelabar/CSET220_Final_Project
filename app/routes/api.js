f47ebab27d765dcbc2be8811fab0300a7fddd733
Cole L Simchick <clsimchick@gmail.com>
2018-11-13 22:05:57 -0500

    Enabled favicon, added a boatload of dependencies, and *actually* completed the Authentication functionality 😭

--- app/routes/api.js
+++ app/routes/api.js
@@ -6,13 +6,17 @@ var config = require('../config'),
   AuthController = require('../controllers/authController'),
   allowOnly = require('../services/routesHelper').allowOnly,
   UserController = require('../controllers/userController'),
-  AdminController = require('../controllers/AdminController');
+  AdminController = require('../controllers/adminController');
 
 var APIRoutes = function(passport) {
 
   router.post('/signup', AuthController.signUp);
 
-  router.post('/login', AuthController.authenticateUser);
+  router.post('/login', AuthController.authenticateUser, function(req, res) {
+    res.redirect('/api/chat')
+  });
+
+  router.get('/api/chat', passport.authenticate('jwt', { session: true }), allowOnly(config.accessLevels.user, UserController.index));
 
   router.get('/profile', passport.authenticate('jwt', { session: false }), allowOnly(config.accessLevels.user, UserController.index));
 
