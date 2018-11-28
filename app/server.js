d83509787b530e3bb540f520839e723ad03c7c49
Cole L Simchick <clsimchick@gmail.com>
2018-11-28 12:48:54 -0500

    Port and host changes for Heroku compatibility

--- app/server.js
+++ app/server.js
@@ -109,7 +109,10 @@ app.get('/products/:id', function (req, res, next) {
   res.json({msg: 'This is CORS-enabled for all origins!'});
 });
 
-app.listen('8080', function () {
+const host = '0.0.0.0';
+const port = process.env.PORT || 8080;
+
+app.listen(port, host, function () {
   console.log('CORS-enabled web server listening on port 8080')
 });
 
