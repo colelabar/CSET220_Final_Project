f65930c8e739b87de9885c0688931be6864dbf7f
Cole L Simchick <clsimchick@gmail.com>
2018-12-06 10:58:45 -0500

    Fixed the validation for emails on signup

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -18,7 +18,7 @@ AuthController.signUp = function(req, res) {
   console.log(req.body.email);
   if(!req.body.email || !req.body.username || !req.body.password) {
     res.json({ message: 'Please provide an email, username, and password.' });
-  } else if(!req.body.email.includes('@%.')) {
+  } else if(!req.body.email.includes('%@%.')) {
     res.json({ message: 'Incorrect email format!' });
   }  else {
     db.sync().then(function() {
